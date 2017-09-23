a7d6039a20fcd939688bd25e80f0121befdbf904
Vimerum <vi.veronese@gmail.com>
2017-09-23 09:10:51 -0300

    Exercicio 04 da Lista 01

---
+++ lista01/04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+	int x, y, resultado;
+
+	printf("Insira um valor: ");
+	scanf("%d", &x);
+	printf("Insira outro valor: ");
+	scanf("%d", &y);
+
+	resultado = x * y;
+
+	printf("Resultado: %d\n", resultado);
+
+	return 0;
+}
