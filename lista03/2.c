51a799d338839317e1ba015f48cc40f7d0ad5c8d
Vimerum <vi.veronese@gmail.com>
2017-10-30 17:49:59 -0200

    Exercicio 2 da Lista 03

---
+++ lista03/2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void inverter (char *str)
+{
+	int size = 0, hSize;
+
+	while (str[size]) {
+		size++;
+	}
+
+	size -= 1;
+	hSize = size / 2;
+
+	for (int i = 0; i <= hSize; i++) {
+		char tmp = str[i];
+		str[i] = str[size-i];
+		str[size-i] = tmp;
+	}
+}
+
+int main ()
+{
+	char str[255];
+
+	fgets(str, 255, stdin);
+
+	printf("%s\n", str);
+
+	inverter(str);
+
+	printf("%s\n", str);
+
+	return 0;
+}
